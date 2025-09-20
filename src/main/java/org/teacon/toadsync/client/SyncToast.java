10f37f213eac040089004c992c1ab4fa63b6f7b1
Yanbing Zhao <zzzz.mail.ustc@gmail.com>
2025-09-20 23:22:27 +0800

    Constant extracting on sync toasts

--- src/main/java/org/teacon/toadsync/client/SyncToast.java
+++ src/main/java/org/teacon/toadsync/client/SyncToast.java
@@ -17,15 +17,17 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public record SyncToast(Component title, Component first, Component second) implements Toast {
     private static final int WIDTH = 160;
     private static final int HEIGHT = 44;
+    private static final int WHITE = 0xFFFFFFFF;
+    private static final int YELLOW = 0xFFFFFF00;
     private static final long VISIBILITY_DURATION = 10000L;
     private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(ToadSync.ID, "toast/sync");
 
     @Override
     public Visibility render(GuiGraphics guiGraphics, ToastComponent component, long timeSinceLastVisible) {
         guiGraphics.blitSprite(TEXTURE, 0, 0, this.width(), this.height());
-        guiGraphics.drawString(component.getMinecraft().font, this.title, 18, 7, 0xFFFFFF00, false);
-        guiGraphics.drawString(component.getMinecraft().font, this.first, 18, 18, 0xFFFFFFFF, false);
-        guiGraphics.drawString(component.getMinecraft().font, this.second, 18, 30, 0xFFFFFFFF, false);
+        guiGraphics.drawString(component.getMinecraft().font, this.title, 18, 7, YELLOW, false);
+        guiGraphics.drawString(component.getMinecraft().font, this.first, 18, 18, WHITE, false);
+        guiGraphics.drawString(component.getMinecraft().font, this.second, 18, 30, WHITE, false);
         var displayTime = VISIBILITY_DURATION * component.getNotificationDisplayTimeMultiplier();
         return timeSinceLastVisible < displayTime ? Visibility.SHOW : Visibility.HIDE;
     }
